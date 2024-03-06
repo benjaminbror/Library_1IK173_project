bb9700fe669edf2ad18ccb29ac0186bd7e6b9168
Carl Britsmar <carl.britsmar@proton.me>
2024-03-06 19:37:19 +0100

    added isSuspended method and isMember method

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -6,21 +6,23 @@ import java.sql.Connection;
 
 
 public class DatabaseManager {
-    public static void main(String[] args) {
+    public void printBookTitles() {
 
-        try{
+    try {
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", "root", "1IK173Lib");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library", 
+        "root", 
+        "1IK173Lib");
 
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery("Select * FROM books");
 
-        while(resultSet.next()) {
+        while (resultSet.next()) {
             System.out.println(resultSet.getString("title"));
         }
-    }catch (Exception e) {
-      e.printStackTrace();
+    } catch (Exception e) {
+        e.printStackTrace();
     }
-    }
+}
 }
