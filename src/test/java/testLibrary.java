502d49a88c178b570b7164abaccbe575ab5fb234
benjaminbror <benjaminbrorsson5@gmail.com>
2024-03-14 20:13:03 +0100

    test class +test

---
+++ src/test/java/testLibrary.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class testLibrary {
+
+
+    @Test
+    public void setMemberMaxBooks(){
+        DatabaseManager databaseManager = new DatabaseManager();
+        Library library = new Library(databaseManager);
+
+        int education_level = 1;
+        int result = library.setMemberMaxBooks(education_level);
+
+        int expected = 3;
+        assertEquals(expected, result);
+
+    }
+}
